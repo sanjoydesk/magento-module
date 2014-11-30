ad7f614fcede1cc644d80f5a0c69ac4dfe376bbe
Sanjoy Dey <sanjoy09@hotmail.com>
2014-11-30 11:56:37 +0530

    Magento- Creating Custom Module

---
+++ code/local/Tutorial/Demo/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+class Tutorial_Demo_IndexController extends Mage_Adminhtml_Controller_Action
+{
+	public function indexAction()
+	{
+			echo "Default Index Controller";exit;
+	}
+	
+    public function testingAction()
+	{
+		echo "Hello Wolrd !!";exit;
+	}
+}
